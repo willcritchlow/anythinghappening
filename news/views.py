30fa539f25a670e4aa8a9c12584bc07fab68ab5b
Will Critchlow <will@distilled.co.uk>
2012-06-06 10:12:45 +0100

    get reddit information on every load

--- news/views.py
+++ news/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render_to_response, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
+from django.core.exceptions import ValidationError
 from news.models import NewsCheck, NewsItem
 import reddit
 from datetime import datetime
 
 @login_required
 def home(request):
+    try:
+        r = reddit.Reddit('willcritchlow anythinginterestingbot')
+        submissions = r.get_subreddit('worldnews').get_top('week')
+        for submission in submissions:
+            ni = NewsItem()
+            ni.slug = submission.id
+            ni.title = submission.title
+            ni.url = submission.url
+            ni.comment_url = submission.permalink
+            ni.created = datetime.fromtimestamp(submission.created_utc)
+            ni.score = submission.score
+            try:
+                ni.validate_unique()
+                ni.save()
+            except ValidationError:
+                print "%s already in database" % ni
+    except:
+        print "Problem getting information from reddit"
     try:
         last_check = NewsCheck.objects.filter(user=request.user).order_by('-time')[0]
         relevant_news = NewsItem.objects.filter(created__gt=last_check.time).order_by('-score')[:5]
@@ -16,24 +35,6 @@ def home(request):
     nc.user = request.user
     nc.save()
     return render_to_response('news/home.html', {'user': request.user, 'news': relevant_news})
-
-@login_required
-def temp(request):
-    r = reddit.Reddit('willcritchlow anythinginterestingbot')
-    submissions = r.get_subreddit('worldnews').get_top('week')
-    for submission in submissions:
-        ni = NewsItem()
-        ni.slug = submission.id
-        ni.title = submission.title
-        ni.url = submission.url
-        ni.comment_url = submission.permalink
-        ni.created = datetime.fromtimestamp(submission.created_utc)
-        ni.score = submission.score
-        try:
-            ni.save()
-        except:
-            print "%s already in database" % ni
-    return redirect("/")
 
 @login_required
 def check(request, nc_id):
