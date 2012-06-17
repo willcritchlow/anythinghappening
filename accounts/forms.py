0dfa3ad68d65973ac9dbfe6f43ecfdf480021664
Will Critchlow <will@distilled.co.uk>
2012-06-17 14:21:00 +0100

    allow user create

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-class UserCreateForm(ModelForm):
+class MyUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(MyUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+        
     class Meta:
         model = User
-        fields = {'username', 'email', 'password'}
+        fields = {'username', 'email', 'first_name', 'last_name'}
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render_to_response, redirect
-from accounts.forms import UserCreateForm
+from accounts.forms import MyUserCreationForm
 from django.template import RequestContext
 from django.contrib.auth import authenticate, login
 
 def create(request):
-
-    return render_to_response('accounts/login.html', {}, context_instance=RequestContext(request))
+    if 'next' in request.GET:
+        next_page = request.GET['next']
+    elif 'next' in request.POST:
+        next_page = request.POST['next']
+    else:
+        next_page = ''
+    if request.method == 'POST':
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            user = authenticate(username=request.POST['username'], password=request.POST['password1'])
+            login(request,user)
+            if next_page:
+                return redirect(next_page)
+            else:
+                return redirect('home')
+    else:
+        form = MyUserCreationForm()
+    return render_to_response('accounts/create.html', {'form': form, 'next': next_page}, context_instance=RequestContext(request))
